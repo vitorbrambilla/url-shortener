52f4f8e6e99b81c9ca5c8977d01371b2117cf0c3
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-03-11 17:57:45 -0300

    changes at the constants

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: 'http://localhost:5000',
   MONGO_CONNETION: 
-    'mongodb+srv://vitor:Dezembro29@url-shortener.uveky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'mongodb+srv://username:<your_password>@url-shortener.uveky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
